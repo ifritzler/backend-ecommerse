7e1ed97ef0df3f3344489ad21537fb3d29a381ff
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-11-03 15:59:07 -0300

    ejs template configure

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/errorHandler.js";
 import apiRouter from "./routes/api.js";
+import ejsConfig from './config/ejs.js'
 
 dotenv.config();
 
 const app = express();
+
+// Templates configure
+ejsConfig(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(process.cwd + "/public"));
@@ -14,7 +18,7 @@ app.use("/api", apiRouter);
 
 app.get("/api/health", (_req, res) => {
   res.status(200).send();
-  res.render()
+  res.render();
 });
 
 app.use(errorHandler);
