31d353cec616be86fa04d5875482000492fe6a37
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-11-10 18:12:43 -0300

    socket-io refactor

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,5 +1,4 @@
 import express from "express";
-import messagesService from "../services/messages.service.js";
 import productService from "../services/products.service.js";
 
 const router = express.Router();
@@ -7,7 +6,6 @@ const router = express.Router();
 router.get("/", async (_req, res, next) => {
   try {
     const products = await productService.all();
-    // const messages = await messagesService.all();
     res.render("index", { products });
   } catch (err) {
     next(err);
