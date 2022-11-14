d30e8acf5e84c2cfcbaf72a24966d9a113c05513
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-11-14 20:45:38 -0300

    update product fields dto

--- src/dto/products.dto.js
+++ src/dto/products.dto.js
@@ -6,7 +6,10 @@ class ProductSave {
     const emptyFields = [];
     if (_.isEmpty(product)) throw new HttpError("Product cannot be empty", 400);
     if (_.isNil(product.title)) emptyFields.push("title");
+    if (_.isNil(product.description)) emptyFields.push("description");
+    if (_.isNil(product.code)) emptyFields.push("code");
     if (_.isNil(product.price)) emptyFields.push("price");
+    if (_.isNil(product.stock)) emptyFields.push("stock");
     if (_.isNil(product.thumbnail)) emptyFields.push("thumbnail");
 
     if (!_.isEmpty(emptyFields))
@@ -16,7 +19,10 @@ class ProductSave {
       );
 
     this.title = product.title;
+    this.description = product.description;
+    this.code = product.code;
     this.price = product.price;
+    this.stock = product.stock;
     this.thumbnail = product.thumbnail;
   }
 }
@@ -26,7 +32,10 @@ class ProductEdit {
     if (_.isEmpty(product)) throw new HttpError("Product cannot be empty", 400);
 
     if(!_.isNil(product.title)) this.title = product.title;
+    if(!_.isNil(product.description)) this.description = product.description;
+    if(!_.isNil(product.code)) this.code = product.code;
     if(!_.isNil(product.price)) this.price = product.price;
+    if(!_.isNil(product.stock)) this.stock = product.stock;
     if(!_.isNil(product.thumbnail)) this.thumbnail = product.thumbnail;
   }
 }
